d24ae61f245722957ff4a2db82a2ccbe64bfe4b0
Clockworks <40514153+Clockworkx@users.noreply.github.com>
2021-08-10 21:36:35 +0200

    temp

--- MS2 Launcher/MainWindow.xaml.cs
+++ MS2 Launcher/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MS2_Launcher
 {
@@ -21,9 +24,34 @@ namespace MS2_Launcher
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(25);
+            timer.Tick += Timer_Tick;
+            CheckOnlineStatus();
+            timer.Start(); 
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            CheckOnlineStatus();
+            timer.Start();
+        }
+
+        private async void CheckOnlineStatus()
+        {
+            var isOnlinePortOpen = await IsPortOpen(Properties.Settings.Default.OnlineIp, Properties.Settings.Default.OnlinePort);
+            var isLocalPortOpen = await IsPortOpen(Properties.Settings.Default.LocalIp, Properties.Settings.Default.LocalPort);
+            if (isOnlinePortOpen) OnlineStatus.Fill = Brushes.Green;
+            else
+            OnlineStatus.Fill = Brushes.Red;
+            if (isLocalPortOpen) LocalStatus.Fill = Brushes.Green;
+            else
+            LocalStatus.Fill = Brushes.Red;
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
@@ -38,18 +66,22 @@ namespace MS2_Launcher
 
         private void MaxmizeButton_Click(object sender, RoutedEventArgs e)
         {
-            //using TcpClient tcpClient = new TcpClient();
-            //try
-            //{
-            //    tcpClient.BeginConnect()
-            //}
         }
 
-        //private bool IsPortOpen(string host, int port, TimeSpan timeout)
-        //{
-        //    using TcpClient tcpClient = new TcpClient();
-        //    tcpClient.BeginConnect(host, port);
-        //}
+        private async Task<bool> IsPortOpen(string host, int port)
+        {
+            using var client = new TcpClient();
+            try
+            {
+                await client.ConnectAsync(host, port);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            
+        }
 
         private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
@@ -58,9 +90,14 @@ namespace MS2_Launcher
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
-           
+           if (Properties.Settings.Default.SelectedServer.Length == 0)
+            {
+                MessageBox.Show("Select a server to start");
+                return;
+            }
+
            System.Diagnostics.Process.Start("C:/Development/MS2/Maplestory 2 Client/appdata/MapleStory2.exe", "127.0.0.1 30000 -ip -port --nxapp=nxl --lc=EN");
-        
+            Close();
         }
     }
 }
